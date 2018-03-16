4f0afb49f985c3183ecfe41acee48fcecdba0caa
Sagar <sagar@betterhalf.ai>
2018-03-16 16:25:08 +0530

    my_profile 1.0

--- App.js
+++ App.js
@@ -1,205 +1,96 @@
-import React from 'react';
-
+import React, { Component } from 'react'
 import {
-  StyleSheet,
-  View,
   Text,
-  Slider,
+  View,
   Image,
-  Platform,
-  ScrollView, TouchableWithoutFeedback
-} from 'react-native';
+  Dimensions
+} from 'react-native'
+import Swiper from 'react-native-swiper'
+const { width } = Dimensions.get('window')
+const loading = require('./Images/loading.gif')
 
-import MultiSlider from '@ptomasroos/react-native-multi-slider';
-import { RadioButtons, SegmentedControls } from 'react-native-radio-buttons';
+const styles = {
+  wrapper: {
+  },
 
-class Example extends React.Component {
-  state = {
-    multiSliderValue: [21, 55],
-    multiSliderValue1: [54, 169],
-    multiSliderValue2: [22, 55],
-  };
+  slide: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'transparent'
+  },
+  image: {
+    backgroundColor: 'transparent',
+    height: 250,
+    borderRadius: 10,
+  },
 
-  multiSliderValuesChange = (values) => {
-    this.setState({
-      multiSliderValue: values,
-    });
-  }
-  multiSliderValuesChange1 = (values) => {
-    this.setState({
-      multiSliderValue1: values,
-    });
-  }
-  multiSliderValuesChange2 = (values) => {
-    this.setState({
-      multiSliderValue2: values,
-    });
-  }
+  loadingView: {
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0,0,0,.5)'
+  },
 
-  render() {
-    const options = [
-      'Yes',
-      'No',
-    ];
-    const option = [
-      'Vegetarian',
-      'Non Vegetarian',
-    ];
-    const opt = [
-      'Never',
-      'Occassionally',
-      'Regularly'
-    ];
-    return (
-      <View style={styles.container}>
-        <Text style={styles.titleText}>Partner Preferences{'\n'}{'\n'}{'\n'}</Text>
-        <View style={styles.sliders}>
-            <View style={styles.sliderOne}>
-              <Text style={styles.text}>Age</Text>
-              <Text style={styles.text}>{this.state.multiSliderValue[0]} - {this.state.multiSliderValue[1]}</Text>
-            </View>
-            <MultiSlider
-              values={[this.state.multiSliderValue[0], this.state.multiSliderValue[1]]}
-              sliderLength={280}
-              onValuesChange={this.multiSliderValuesChange}
-              min={0}
-              max={100}
-              step={1}
-              selectedStyle={{backgroundColor: '#0277bd',}}
-              markerStyle={{backgroundColor: '#0277bd',}}
-              // containerStyle={{height:40,}}
-              allowOverlap
-              snapped
-            />
-            <View style={styles.sliderOne}>
-              <Text style={styles.text}>Height</Text>
-              <Text style={styles.text}>{this.state.multiSliderValue1[0]} cm - {this.state.multiSliderValue1[1]} cm</Text>
-            </View>
-            <MultiSlider
-              values={[this.state.multiSliderValue1[0], this.state.multiSliderValue1[1]]}
-              sliderLength={280}
-              onValuesChange={this.multiSliderValuesChange1}
-              min={0}
-              max={100}
-              step={1}
-              selectedStyle={{backgroundColor: '#0277bd',}}
-              markerStyle={{backgroundColor: '#0277bd',}}
-              // containerStyle={{height:40,}}
-              allowOverlap
-              snapped
-            />
-            <View style={styles.sliderOne}>
-              <Text style={styles.text}>Monthly Income</Text>
-              <Text style={styles.text}>{this.state.multiSliderValue2[0]} k - {this.state.multiSliderValue2[1]} k </Text>
-              <Text style={styles.text}></Text>
-            </View>
-            <MultiSlider
-              values={[this.state.multiSliderValue2[0], this.state.multiSliderValue2[1]]}
-              sliderLength={280}
-              onValuesChange={this.multiSliderValuesChange2}
-              min={0}
-              max={100}
-              step={1}
-              selectedStyle={{backgroundColor: '#0277bd',}}
-              markerStyle={{backgroundColor: '#0277bd',}}
-              // containerStyle={{height:40,}}
-              allowOverlap
-              snapped
-            />
-        </View>
-        <View style={{padding: 20}}>
-          <View style={{flexDirection:'row',justifyContent:'space-between'}}>
-            <Text>Same location</Text>
-              <SegmentedControls
-                containerStyle={{width:160}}
-                options={ options }
-              />
-          </View>
-          <View style={{flexDirection:'row',justifyContent:'space-between', paddingTop: 10}}>
-            <Text>Same religion</Text>
-              <SegmentedControls
-                containerStyle={{width:160}}
-                options={ options }
-              />
-          </View>
-          <View style={{flexDirection:'row',justifyContent:'space-between', paddingTop: 10}}>
-            <Text>Same mother tongue</Text>
-              <SegmentedControls
-                containerStyle={{width:160}}
-                options={ options }
-              />
-          </View>
-          <View style={{flexDirection:'row',justifyContent:'space-between', paddingTop: 10}}>
-            <Text>Sagar</Text>
-              <SegmentedControls
-                containerStyle={{width:160}}
-                options={ options }
-              />
-          </View>
-          <View style={{flexDirection:'row',justifyContent:'space-between', paddingTop: 10}}>
-            <Text>Same caste</Text>
-              <SegmentedControls
-                containerStyle={{width:160}}
-                options={ options }
-              />
-          </View>
-          <View style={{flexDirection:'row',justifyContent:'space-between', paddingTop: 10}}>
-            <Text>Food preferences</Text>
-              <SegmentedControls
-                containerStyle={{width:180}}
-                optionStyle={{fontSize: 12,}}
-                options={ option }
-              />
-          </View>
-          <View style={{flexDirection:'row',justifyContent:'space-between', paddingTop: 10}}>
-            <Text>Smoke</Text>
-              <SegmentedControls
-                containerStyle={{width:240}}
-                optionStyle={{fontSize: 12,}}
-                options={ opt }
-              />
-          </View>
-          <View style={{flexDirection:'row',justifyContent:'space-between', paddingTop: 10}}>
-            <Text>Drink</Text>
-              <SegmentedControls
-                containerStyle={{width:240}}
-                optionStyle={{fontSize: 12,}}
-                options={ opt }
-              />
-          </View>
-        </View>
-      </View>
-    );
+  loadingImage: {
+    width: 60,
+    height: 60
   }
 }
 
-export default Example;
+const Slide = props => {
+  return (<View style={styles.slide}>
+    <Image onLoad={props.loadHandle.bind(null, props.i)} style={styles.image} source={{uri: props.uri}} />
+    {
+      !props.loaded && <View style={styles.loadingView}>
+        <Image style={styles.loadingImage} source={loading} />
+      </View>
+    }
+  </View>)
+}
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 10,
-  },
-  sliders: {
-    paddingLeft: 20,
-    width: 280,
-    height: 200,
-  },
-  text: {
-    alignSelf: 'center',
-    paddingBottom: 20,
-  },
-  sliderOne: {
-    flex: 2,
-    flexDirection: 'row',
-    justifyContent: 'space-around'
-  },
-  titleText: {
-    flex: 1,
-    fontSize: 25,
-    fontWeight: '100',
-    color: '#0277bd',
-    paddingTop: 20,
-    paddingLeft: 20,
-  },
-});
+export default class extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      imgList: [
+        'https://gitlab.pro/yuji/demo/uploads/d6133098b53fe1a5f3c5c00cf3c2d670/DVrj5Hz.jpg_1',
+        'https://gitlab.pro/yuji/demo/uploads/2d5122a2504e5cbdf01f4fcf85f2594b/Mwb8VWH.jpg',
+        'https://gitlab.pro/yuji/demo/uploads/4421f77012d43a0b4e7cfbe1144aac7c/XFVzKhq.jpg',
+        'https://gitlab.pro/yuji/demo/uploads/576ef91941b0bda5761dde6914dae9f0/kD3eeHe.jpg'
+      ],
+      loadQueue: [0, 0, 0, 0]
+    }
+    this.loadHandle = this.loadHandle.bind(this)
+  }
+  loadHandle (i) {
+    let loadQueue = this.state.loadQueue
+    loadQueue[i] = 1
+    this.setState({
+      loadQueue
+    })
+  }
+  render () {
+    return (
+
+          <View style={{flex: 1, paddingBottom: 250, paddingLeft: 10, paddingRight: 10}}>
+            <Swiper loadMinimal loadMinimalSize={1} style={styles.wrapper} loop={false}>
+              {
+                this.state.imgList.map((item, i) => <Slide
+                  loadHandle={this.loadHandle}
+                  loaded={!!this.state.loadQueue[i]}
+                  uri={item}
+                  i={i}
+                  key={i} />)
+              }
+            </Swiper>
+            <View>
+              <Text>Current Loaded Images: {this.state.loadQueue}</Text>
+            </View>
+        </View>
+    )
+  }
+}
